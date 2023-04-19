9c5c9324ab531a75ee8b6ed8ad577f5c88152966
ajay.k.gupta <ajay.k.gupta@innovaccer.com>
2023-04-19 22:44:58 +0530

    python assignment-1:merge two list

--- main.py
+++ main.py
@@ -26,7 +26,21 @@ def merge_lists(list_1, list_2) -> list:
     - Both lists are unsorted
     - Both lists can have missing values (for ex list_2 has missing id=2)
     """
-    # return list_3
+    merged = []
+    merged.extend(list_1)
+    list_1.extend(list_2)
+    set_of_id = set()
+    for d1 in list_1:
+        for d2 in list_2:
+            if d1.get("id") not in set_of_id and d1.get("id") == d2.get("id"):
+                # if the id is not present in set_of_id and both the id of d1 and d2 matches
+                set_of_id.add(d1.get("id"))
+                d1.update(d2)
+                if d1 not in merged:
+                    merged.append(d1)
+                break
+    return merged
 
 
 list_3 = merge_lists(list_1, list_2)
+print(list_3)
